0d24dc9b6b8fb4ae5c618ee50e9a539169361685
Nicolas Alvaredo <nicoalvaredo_utn@hotmail.com>
2024-03-06 18:07:10 -0300

    update JS

--- js/main.js
+++ js/main.js
@@ -9,38 +9,35 @@ function calcularIMC(peso, altura) {
 // Función para validar si los valores ingresados son  números y no otros caracteres.
 // Uso el simbolo ! para invertir la logica
 
-function validarNumero (numero){
+function validarNumero(numero) {
     return !isNaN(numero) && isFinite(numero);
 }
 
 /* Función principal */
 function calcularIMCpersona() {
     let arrayPersonas = [];
-    let nombre = ""
-    let peso = 0
-    let altura = 0
     let cantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas para las que desea calcular el Indice de masa corporal:"));
 
     // Validar que la cantidad de personas ingresada sea un número válido
     if (cantidadPersonas <= 0 || !validarNumero(cantidadPersonas)) {
         alert("La cantidad de personas debe ser un número válido mayor que cero.");
-        
+
     }
 
     // Pedir datos para cada persona y calcular su IMC
     for (let i = 0; i < cantidadPersonas; i++) {
-        let nombre = prompt("Ingrese el nombre de la persona " + (i + 1) );
+        let nombre = prompt("Ingrese el nombre de la persona " + (i + 1));
         let peso = parseFloat(prompt("Ingrese el peso de la persona " + (i + 1) + " en kilogramos:"));
         let altura = parseFloat(prompt("Ingrese la altura de la persona " + (i + 1) + " en metros:"));
 
         // Validar que los datos ingresados sean números válidos
         if (peso <= 0 || altura <= 0 || !validarNumero(peso) || !validarNumero(altura)) {
             alert("Los valores ingresados para el peso y la altura deben ser números válidos mayores que cero.");
-            
+
         }
 
         let IMC = calcularIMC(peso, altura);
-        let mensaje = "";
+        
 
         // Determinar el mensaje según el rango de IMC
         switch (true) {
@@ -56,17 +53,29 @@ function calcularIMCpersona() {
         }
 
         // Mostrar el resultado del IMC y el mensaje de estado
-        alert(  nombre + " tiene un IMC de " + IMC.toFixed(1) + "." + "\nEstá " + mensaje);
+        alert(nombre + " tiene un IMC de " + IMC.toFixed(1) + "." + "\nEstá " + mensaje);
 
 
-// Agregar los datos de IMC al array de personas
-arrayPersonas.push({nombre: nombre, IMC: IMC});
+        // Agregar los datos de IMC al array de personas
+        arrayPersonas.push({
+            nombre: nombre,
+            IMC: IMC
+        });
 
     }
 
-    console.table(arrayPersonas);
+    // Mostrar los resultados en el HTML
+    arrayPersonas.forEach(persona => {
+        let nuevoelementoLi = document.createElement('li');
+        nuevoelementoLi.innerHTML = `ID:${persona.nombre} - IMC: ${persona.IMC.toFixed(1)}`;
+        listado.appendChild(nuevoelementoLi);
+    });
+
+    // Guardar los datos en el localStorage
+    const IMCJSON = JSON.stringify(arrayPersonas)
+    localStorage.setItem('arrayPersonas',IMCJSON );
 }
 
-// Inicio de calculo de IMC por llamado de funcion principal
-calcularIMCpersona();
-
+// Obtener el botón y agregar el evento click
+let IMCboton = document.getElementById("calcularIMCButton")
+IMCboton.addEventListener("click", calcularIMCpersona);
